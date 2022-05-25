bef5023f5cc2fa4ea7f3d5df406d41bf42e8ea64
MattiaBattecca <mattia.battecca.studenti@isii.it>
2022-05-25 16:45:46 +0200

    aggiunta visualizzazione clienti

--- clienteG.php
+++ clienteG.php
@@ -6,27 +6,35 @@
 // }
 //include('template_header.php');
 include('dal_studio.php');
-$id = $_GET['CodiceFiscale'];
-$personaF = select_all_clienti('persona_fisica');
 ?>
 
 <?php
-if($SESSION['Tipo'] == 'Avvocato')
-{
-
-
-}
-else if ($SESSION['Tipo'] == 'Commercialista')
-{
-
-
-}
-else if ($SESSION['Tipo'] == 'Consulente')
-{
-
-
-}
+$id = $SESSION['Id'];
+$tipo = trova_tipo_utente($id); //non so se serve
+$personaG = trova_clientiG_di($id);//SELECT * FROM persona_giuridica WHERE persona_giuridica.NumeroPartitaIVA IN (SELECT consulenza.PIGiuridica FROM consulenza WHERE consulenza.CFDipendente LIKE "")
 ?>
+
+
+
+<h2>Società di capitali e società di persone</h2>
+<table>
+    <tr>
+        <th>Partita IVA</th>
+        <th>Nome</th>
+        <th>Sede</th>
+    </tr>
+    <?php
+    foreach ($personaG as $row) {
+    ?>
+        <tr>
+            <td><a href="clienteG.php?NumeroPartitaIVA=<?= $row['NumeroPartitaIVA'] ?>"><?= $row['NumeroPartitaIVA'] ?></a></td>
+            <td><?= $row['Nome'] ?></td>
+            <td><?= $row['Sede'] ?></td>
+        </tr>
+    <?php
+    }
+    ?>
+</table>
 
 <?php
 include('template_footer.php');
